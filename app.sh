28d3bb3773e5f32c4b4a6d80465ed976354a3c47
abishek1062 <abishek.s@outlook.com>
2020-10-26 23:06:22 +0530

    app.sh typo

--- app.sh
+++ app.sh
@@ -1,5 +1,5 @@
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 export FLASK_APP=run.py
-pip instasll -r requirements_apps.txt --user
+pip install -r requirements_apps.txt --user
 python -m flask run --host=0.0.0.0 --port=8888
